a59dc8faafdf0cf370eba546c6b1009e7ff040d5
João Henrique Franco <joaohenriquef@poli.ufrj.br>
2021-03-10 22:50:59 -0300

    Add maps file

---
+++ src/screens/maps.js
@@ -0,0 +1,44 @@
+export const maps = [{
+  rows: 2,
+  columns: 4,
+  postions: [
+    [0, 0],
+    [0, 1],
+    [0, 2],
+    [0, 3],
+    [1, 0],
+  ],
+  connections: [
+    [
+      [0, 0],
+      [0, 1, 1],
+    ], //a conexão é entre duas posições, sendo [saida]para[entrada],
+    [
+      [0, 1],
+      [0, 2, 1],
+    ], //[entrada] tem um terceiro indice para a porta: 0->em cima, 1->abaixo
+    [
+      [0, 2],
+      [0, 3, 0],
+    ],
+    [
+      [1, 0],
+      [0, 3, 1],
+    ],
+    //[[0,3],[1,3,1]]
+  ],
+  lamp: [1, 3], //posição da lâmpada
+  solution: (allFunc) => {
+    if (!allFunc) {
+      return false;
+    }
+    const { f1, f2, f3, f4 } = allFunc;
+
+    if (f1 && f2 && f3 && f4) {
+      return () => !!f3(f4(1,1),f2(f1(1,1),1))
+    }
+  },
+  gates: ["AND1", "AND1", "OR0", "AND"],
+},
+
+]
